f466598f867ced79c95ae4492f3887ad8241953f
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-01-08 00:00:57 +0100

    altering code to updated yahoo_finance library

--- monthly_update/update_security_prices.py
+++ monthly_update/update_security_prices.py
@@ -22,9 +22,7 @@ for index, (security_id, ticker) in enumerate(securities):
     try:
         reader = YahooReader(ticker=ticker).historical_data(
             frequency="1d",
-            start="1900-01-01",
-            timestamps=False,
-            rounded=False
+            timestamps=True
         )
         df = reader["data"]
     except:
@@ -33,20 +31,21 @@ for index, (security_id, ticker) in enumerate(securities):
         continue
 
     currency = reader["information"]["currency"]
+    offset = reader["information"]["utc_offset"]
     try:
         currency_id = cur.execute("SELECT id FROM currencies WHERE abbr = ?", (currency, )).fetchone()[0]
     except:
         print("\t no currency", currency)
         continue
-    df.index = [((date - dt.date(1970,1,1)).total_seconds()) for date in df.index]
     df["security id"] = security_id
-    df["date"] = df.index
-    data = df.reindex(columns = ["security id", "date", "open", "high", "low", "close", "adj close", "volume", "dividends", "stock splits", "simple returns", "log returns"]).values
+    df["ts"] = df.index
+    data = df.reindex(columns = ["security id", "ts", "open", "high", "low", "close", "adj_close", "volume", "dividends", "splits", "simple_returns", "log_returns"]).values
+
     cur.executemany(
         "REPLACE INTO security_prices VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
         (data)
     )
-    cur.execute("UPDATE securities SET prices_updated = ?, currency_id = ? WHERE id = ?", (ts_today, currency_id, security_id))
+    cur.execute("UPDATE securities SET prices_updated = ?, currency_id = ?, utc_offset = ? WHERE id = ?", (ts_today, currency_id, offset, security_id))
     if index % 100 == 0:
         con.commit()
 
