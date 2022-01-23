75a03868989b160981f70dde7377503f39eb9fb6
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-01-23 22:46:56 +0100

    fixing quarter computation bug

--- monthly_update/insert_macrotrends_fundamentals_quarterly.py
+++ monthly_update/insert_macrotrends_fundamentals_quarterly.py
@@ -15,6 +15,11 @@ tickers = [ticker.strip(".json") for ticker in tickers]
 for index, ticker in enumerate(tickers):
     print(f"{ticker}, {index} of {len(tickers)}")
     security_id = cur.execute("SELECT id FROM securities WHERE ticker = ?", (ticker,)).fetchone()[0]
+    try:
+        fiscal_year_end = cur.execute("SELECT ts FROM fundamental_data_macrotrends WHERE security_id = ? AND quarter = 0", (security_id,)).fetchone()[0]
+    except:
+        continue
+    fiscal_year_end_quarter = pd.to_datetime(fiscal_year_end, unit="s").quarter
     with open(fr"fundamentals/quarterly/{ticker}.json", "r") as f:
         dct = json.load(f)
     for statement in dct.keys():
@@ -26,13 +31,7 @@ for index, ticker in enumerate(tickers):
                 date = pd.to_datetime(date)
                 year = date.year
                 timestamp =  int(date.timestamp())
-                try:
-                    fiscal_year_end = cur.execute("SELECT ts FROM fundamental_data_macrotrends WHERE security_id = ? AND quarter = 0", (security_id,)).fetchone()[0]
-                except:
-                    break
-                fiscal_year_end = pd.to_datetime(fiscal_year_end, unit="s")
-                quarter = int(((date.month-fiscal_year_end.month)/3 + 3) % 4 + 1)
-                date.month - fiscal_year_end.month
+                quarter = (date.quarter+3-fiscal_year_end_quarter)%4+1
                 financial_data.append((security_id, var_id, quarter, year, timestamp, value))
             cur.executemany("REPLACE INTO fundamental_data_macrotrends VALUES (?, ?, ?, ?, ?, ?)", financial_data)
     
