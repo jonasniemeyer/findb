473d5a893380789b7958700d8ff6291c3c515536
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-01-23 20:33:12 +0100

    fix insert bugs

--- weekly_update/update_commodity_data.py
+++ weekly_update/update_commodity_data.py
@@ -1,11 +1,16 @@
 from finance_database import Database
 from finance_data import CMEReader
 import pandas as pd
+import numpy as np
+from sqlite3 import register_adapter
 import datetime as dt
+
+register_adapter(np.int64, lambda val: int(val))
+register_adapter(np.float64, lambda val: float(val))
 
 ts_today = int(pd.to_datetime(pd.to_datetime("today").date()).timestamp())
 
-if dt.date.today().weekday() == 5:
+if dt.date.today().weekday() == 6:
 
     db = Database()
     con = db.connection
@@ -16,8 +21,6 @@ if dt.date.today().weekday() == 5:
         data[commodity] = CMEReader(commodity, timestamps=True).read()
 
     for commodity in data.keys():
-        if commodity != "Cotton":
-            continue
         commodity_id = cur.execute("SELECT id FROM commodities WHERE name = ?", (commodity,)).fetchone()[0]
         for ts in data[commodity].keys():
             df = data[commodity][ts]
@@ -25,7 +28,9 @@ if dt.date.today().weekday() == 5:
             df["maturity_ts"] = df.index
             df["ts"] = ts
             df = df[["commodity_id", "maturity_ts", "ts", "Settle", "Volume", "Open Interest"]].to_numpy()
-            cur.executemany("REPLACE INTO commodit_prices VALUES (?, ?, ?, ?, ?, ?)", df)
+            cur.executemany("REPLACE INTO commodity_prices VALUES (?, ?, ?, ?, ?, ?)", df)
+        
+        cur.execute("UPDATE commodities SET prices_updated = ? WHERE name = ?", (ts_today, commodity))
 
     con.commit()
     con.close()
