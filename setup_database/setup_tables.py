b5cf9069d7b6368a75ce7482ca783ea400ea09d6
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-01-23 22:46:30 +0100

    changing primary key fundamental data tables

--- setup_database/setup_tables.py
+++ setup_database/setup_tables.py
@@ -364,7 +364,7 @@ cur.execute(
         ts INTEGER,
         value INTEGER,
         filing_id INTEGER,
-        PRIMARY KEY(security_id, quarter, year, variable_id)
+        PRIMARY KEY(security_id, variable_id, ts)
     )
     """
 )
@@ -389,7 +389,7 @@ cur.execute(
         year INTEGER,
         ts INTEGER,
         value INTEGER,
-        PRIMARY KEY(security_id, variable_id, quarter, year)
+        PRIMARY KEY(security_id, variable_id, ts)
     )
     """
 )
@@ -414,7 +414,7 @@ cur.execute(
         year INTEGER,
         ts INTEGER,
         value INTEGER,
-        PRIMARY KEY(security_id, variable_id, quarter, year)
+        PRIMARY KEY(security_id, variable_id, ts)
     )
     """
 )
