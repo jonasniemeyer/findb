46451d651dd5509406b761e880b2f7ff15b7eefc
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-11-15 10:43:01 +0100

    rm commodities from utils, import from CMEReader instead

--- setup_database/commodities.py
+++ setup_database/commodities.py
@@ -1,12 +1,12 @@
-from finance_database.utils import COMMODITIES
+from finance_data import CMEReader
 from finance_database import Database
 
 db = Database()
 con = db.connection
 cur = db.cursor
 
-for name, properties in COMMODITIES.items():
-    sector_id = cur.execute(f"SELECT id FROM cme_commodity_sectors WHERE name = ?", (properties['sector name'],)).fetchone()[0]
+for name, properties in CMEReader.commodities.items():
+    sector_id = cur.execute(f"SELECT id FROM cme_commodity_sectors WHERE name = ?", (properties["sector_name"],)).fetchone()[0]
     exchange_id = cur.execute(f"SELECT id FROM yahoo_exchanges WHERE yahoo_suffix = '.CME'").fetchone()[0]
     cur.execute(
         f"INSERT OR IGNORE INTO cme_commodities (name, exchange_id, sector_id) VALUES (?, ?, ?)",
