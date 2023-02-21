f0b65369049771e929ad020bb7a9fb6d6b3018f2
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-02-21 21:51:24 +0100

    add security_prices method

--- db.py
+++ db.py
@@ -1,13 +1,37 @@
 from finance_database.utils import DB_PATH
 from sqlite3 import connect
+from typing import Union
+import pandas as pd
 
 class Database:    
     def __init__(self):
         self.connection = connect(DB_PATH)
+        self.con = self.connection
         self.cursor = self.connection.cursor()
+        self.cur = self.cursor
         
     def __enter__(self):
         return self
     
     def __exit__(self, *args):
         self.connection.close()
+    
+    def security_prices(
+        self,
+        ticker,
+        start="1900-01-01",
+        end=pd.to_datetime("today").isoformat()
+    ) -> Union[dict, list]:
+        query = """
+        SELECT ts, adj_close FROM yahoo_security_prices 
+        WHERE security_id = (SELECT id FROM securities WHERE ticker = ?)
+        AND ts BETWEEN STRFTIME("%s", ?) AND STRFTIME("%s", ?)
+        """
+
+        if isinstance(ticker, str):
+            return self.cur.execute(query, (ticker, start, end)).fetchall()
+        else:
+            data = {}
+            for symbol in ticker:
+                data[symbol] = self.cur.execute(query, (symbol, start, end)).fetchall()
+            return data
