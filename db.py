3e78b245a6146879c171427ff08dca7e67e5b1fe
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-02-21 22:16:18 +0100

    add security_news method

--- db.py
+++ db.py
@@ -30,3 +30,26 @@ class Database:
             """,
             (ticker, start, end)).fetchall()
         return data
+
+    def security_news(
+        self,
+        ticker,
+        start="1900-01-01",
+        end=pd.to_datetime("today").isoformat()
+    ) -> list:
+        data = self.cur.execute(
+            """
+            SELECT
+            source.name,
+            news.header,
+            news.description,
+            news.url
+            FROM security_news news 
+            INNER JOIN security_news_match match
+            INNER JOIN news_source source
+            ON news.id = match.news_id AND news.source_id = source.id
+            WHERE match.security_id = (SELECT id FROM securities WHERE ticker = ?)
+            AND news.ts BETWEEN STRFTIME("%s", ?) AND STRFTIME("%s", ?)
+            """,
+            (ticker, start, end)).fetchall()
+        return data
