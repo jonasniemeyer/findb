909a58a6641b4aed70c2b804bc42dbca78e85ba0
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-02-21 21:19:32 +0100

    make Database contextmanager compliant

--- db.py
+++ db.py
@@ -1,9 +1,13 @@
 from finance_database.utils import DB_PATH
 from sqlite3 import connect
 
-class Database:
-    path = DB_PATH
-    
+class Database:    
     def __init__(self):
-        self.connection = connect(self.path)
+        self.connection = connect(DB_PATH)
         self.cursor = self.connection.cursor()
+        
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, *args):
+        self.connection.close()
