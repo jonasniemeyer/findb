4e03a4506ea438a2641f6eb174b1430ee3215fdc
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-02-21 23:08:16 +0100

    add competitors method

--- db.py
+++ db.py
@@ -40,16 +40,57 @@ class Database:
         data = self.cur.execute(
             """
             SELECT
-            source.name,
-            news.header,
-            news.description,
-            news.url
-            FROM security_news news 
-            INNER JOIN security_news_match match
-            INNER JOIN news_source source
-            ON news.id = match.news_id AND news.source_id = source.id
-            WHERE match.security_id = (SELECT id FROM securities WHERE ticker = ?)
-            AND news.ts BETWEEN STRFTIME("%s", ?) AND STRFTIME("%s", ?)
+            news_source.name,
+            security_news.header,
+            security_news.description,
+            security_news.url
+            FROM security_news INNER JOIN security_news_match INNER JOIN news_source
+            ON security_news.id = security_news_match.news_id AND security_news.source_id = news_source.id
+            WHERE security_news_match.security_id = (SELECT id FROM securities WHERE ticker = ?)
+            AND security_news.ts BETWEEN STRFTIME("%s", ?) AND STRFTIME("%s", ?)
             """,
             (ticker, start, end)).fetchall()
+        return data
+    
+    def competitors(
+        self,
+        ticker,
+        classification="gics"
+    ) -> list:
+        if classification.lower() == "gics":
+            data = self.cur.execute(
+                """
+                SELECT
+                ticker,
+                yahoo_name
+                FROM securities
+                WHERE id IN (
+                    SELECT security_id FROM companies WHERE gics_industry_id = (
+                        SELECT gics_industry_id FROM companies WHERE security_id = (
+                            SELECT id FROM securities WHERE ticker = ?
+                        )
+                    )
+                )
+                AND ticker NOT LIKE ?
+                ORDER BY ticker
+                """,
+                (ticker, ticker+"%")).fetchall()
+        else:
+            data = self.cur.execute(
+                """
+                SELECT
+                ticker,
+                yahoo_name
+                FROM securities
+                WHERE id IN (
+                    SELECT security_id FROM companies WHERE sic_industry_id = (
+                        SELECT sic_industry_id FROM companies WHERE security_id = (
+                            SELECT id FROM securities WHERE ticker = ?
+                        )
+                    )
+                )
+                AND ticker NOT LIKE ?
+                ORDER BY ticker
+                """,
+                (ticker, ticker+"%")).fetchall()
         return data
