97b3b3a6f3d44b0f5580f048899def071b7fef3d
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-02-27 17:32:50 +0100

    update to singular naming convention

--- weekly_update/update_commodity_data.py
+++ weekly_update/update_commodity_data.py
@@ -23,16 +23,16 @@ if dt.date.today().weekday() == 6:
         data[commodity] = CMEReader(commodity, timestamps=True).read()
 
     for commodity in data.keys():
-        commodity_id = cur.execute("SELECT id FROM cme_commodities WHERE name = ?", (commodity,)).fetchone()[0]
+        commodity_id = cur.execute("SELECT commodity_id FROM cme_commodity WHERE name = ?", (commodity,)).fetchone()[0]
         for ts in data[commodity].keys():
             df = data[commodity][ts]
             df["commodity_id"] = commodity_id
             df["maturity_ts"] = df.index
             df["ts"] = ts
             df = df[["commodity_id", "maturity_ts", "ts", "Settle", "Volume", "Open Interest"]].to_numpy()
-            cur.executemany("REPLACE INTO cme_commodity_prices VALUES (?, ?, ?, ?, ?, ?)", df)
+            cur.executemany("REPLACE INTO cme_commodity_data VALUES (?, ?, ?, ?, ?, ?)", df)
         
-        cur.execute("UPDATE cme_commodities SET prices_updated = ? WHERE name = ?", (ts_today, commodity))
+        cur.execute("UPDATE cme_commodity SET prices_updated = ? WHERE name = ?", (ts_today, commodity))
 
     con.commit()
     con.close()
