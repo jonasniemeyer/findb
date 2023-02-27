bff2f8a5643891b726b9f92eca4324f557295653
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-02-27 17:31:44 +0100

    update to singular naming convention

--- monthly_update/insert_macrotrends_fundamentals_yearly.py
+++ monthly_update/insert_macrotrends_fundamentals_yearly.py
@@ -14,12 +14,12 @@ tickers = [ticker.strip(".json") for ticker in tickers]
 
 for index, ticker in enumerate(tickers):
     print(f"{ticker}, {index} of {len(tickers)}")
-    security_id = cur.execute("SELECT id FROM securities WHERE ticker = ?", (ticker,)).fetchone()[0]
+    security_id = cur.execute("SELECT security_id FROM security WHERE ticker = ?", (ticker,)).fetchone()[0]
     with open(fr"fundamentals/yearly/{ticker}.json", "r") as f:
         dct = json.load(f)
     for statement in dct.keys():
         for variable, data in dct[statement].items():
-            var_id = cur.execute("SELECT id from macrotrends_fundamental_variables WHERE name = ?", (variable,)).fetchone()[0]
+            var_id = cur.execute("SELECT id from macrotrends_fundamental_variable WHERE name = ?", (variable,)).fetchone()[0]
             data = data.items()
             financial_data = []
             for date, value in data:
@@ -29,7 +29,7 @@ for index, ticker in enumerate(tickers):
                 financial_data.append((security_id, var_id, 0, year, timestamp, value))
             cur.executemany("REPLACE INTO macrotrends_fundamental_data VALUES (?, ?, ?, ?, ?, ?)", financial_data)
     
-    cur.execute("UPDATE companies SET macrotrends_fundamentals_updated = ?, fiscal_year_end = ? WHERE security_id = ?", (timestamp_today, date.month, security_id))
+    cur.execute("UPDATE company SET macrotrends_fundamentals_updated = ?, fiscal_year_end = ? WHERE security_id = ?", (timestamp_today, date.month, security_id))
     
 con.commit()
 con.close()
