a587eff0549a2c0edf5620d4f2a7f25fa64a7140
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-03-02 09:21:11 +0100

    add Stratosphere variable mapping

--- utils.py
+++ utils.py
@@ -19,14 +19,19 @@ class Conversion(Enum):
     # Income Statement
     REVENUE = "Total Revenue"
     REVENUE_ANALYSTS = "Total Revenue Analyst Count"
-    COGS = "Cost Of Revenue"
+    COGS = "Cost of Revenue"
     GROSS_PROFIT = "Gross Profit"
     RD = "Research & Development Expenses"
+    GA = "General & Administrative Expenses"
+    SM = "Selling & Marketing Expenses"
     SGA = "Selling, General & Administrative Expenses"
     OTHER_OPERATING_INCOME = "Other Operating Income/Expenses"
     TOTAL_OPERATING_EXPENSES = "Total Operating Expenses"
+    TOTAL_EXPENSES = "Total Expenses"
     OPERATING_INCOME = "Total Operating Income"
     OPERATING_INCOME_ANALYSTS = "Total Operating Income Analyst Count"
+    INTEREST_INCOME = "Interest Income"
+    INTEREST_EXPENSE = "Interest Expense"
     NONOPERATING_INCOME = "Non-Operating Income/Loss"
     PRETAX_INCOME = "Income Before Income Taxes"
     PRETAX_INCOME_ANALYSTS = "Income Before Income Taxes Analyst Count"
@@ -49,8 +54,9 @@ class Conversion(Enum):
     DIVIDENDS_PER_SHARE_ANALYSTS = "Dividends Per Share Analyst Count"
 
     # Balance Sheet
-    CASH = "Cash And Cash Equivalents"
+    CASH = "Cash and Cash Equivalents"
     ST_SECURITIES = "Short-Term Marketable Securities"
+    CASH_ST_SECURITIES = "Cash and Short-Term Marketable Securities"
     ACCOUNTS_RECEIVABLE = "Accounts Receivable"
     INVENTORY = "Inventory"
     PREPAID_EXPENSES = "Prepaid Expenses"
@@ -62,52 +68,73 @@ class Conversion(Enum):
     GOODWILL = "GoodWill"
     INTANGIBLE_ASSETS = "Intangible Assets"
     GOODWILL_INTANGIBLES = "Goodwill and Intangible Assets"
+    TAX_ASSETS = "Tax Assets"
     OTHER_NONCURRENT_ASSETS = "Other Non-Current Assets"
     TOTAL_NONCURRENT_ASSETS = "Total Non-Current Assets"
 
+    TOTAL_INVESTMENTS ="Total Investments"
+    OTHER_ASSETS = "Other Assets"
     TOTAL_ASSETS = "Total Assets"
     TOTAL_ASSETS_ANALYSTS = "Total Assets Analyst Count"
 
     ST_DEBT = "Short-Term Debt"
+    TAX_LIABILITIES = "Tax Liabilities"
     CURRENT_LIABILITIES = "Current Liabilities"
+    ACCOUNTS_PAYABLE = "Accounts Payable"
+    ST_DEFERRED_REVENUE = "Short-Term Deferred Revenue"
+    OTHER_CURRENT_LIABILITIES = "Other Current Liabilities"
+    TOTAL_CURRENT_LIABILITIES = "Total Current Liabilities"
     LT_DEBT = "Long-Term Debt"
+    LT_DEFERRED_REVENUE = "Long-Term Deferred Revenue"
+    LT_DEFERRED_TAX_LIABILITIES = "Long-Term Deferred Tax Liabilities"
     OTHER_NONCURRENT_LIABILITIES = "Other Non-Current Liabilities"
     TOTAL_NONCURRENT_LIABILITIES = "Total Non-Current Liabilities"
+    OTHER_LIABILITIES = "Other Liabilities"
+    CAPITAL_LEASE_OBLIGATIONS = "Capital Lease Obligations"
+    TOTAL_DEBT = "Total Debt"
+    NET_DEBT = "Net Debt"
     TOTAL_LIABILITIES = "Total Liabilities"
 
     COMMON_STOCK = "Common Stock"
+    PREFERRED_STOCK = "Preferred Stock"
     RETAINED_EARNINGS = "Retained Earnings"
     COMPREHENSIVE_INCOME = "Comprehensive Income"
     OTHER_SHAREHOLDERS_EQUITY = "Other Shareholders' Equity"
     TOTAL_SHAREHOLDERS_EQUITY = "Total Shareholders' Equity"
     TOTAL_SHAREHOLDERS_EQUITY_ANALYSTS = "Total Shareholders' Equity Analyst Count"
-    BOOK_VALUE_PER_SHARE = "Book Value Per Share"
-    BOOK_VALUE_PER_SHARE_ANALYSTS = "Book Value Per Share Analyst Count"
+    TOTAL_EQUITY = "Total Equity"
+    MINORITY_INTEREST = "Minority Interest"
 
     TOTAL_LIABILITIES_AND_SHAREHOLDERS_EQUITY = "Total Liabilities And Shareholders' Equity"
+    TOTAL_LIABILITIES_AND_EQUITY = "Total Liabilities And Equity"
 
     # Cashflow Statement
     NET_INCOME_CF = "Net Income"
     DA = "Depreciation and Amortization"
-    CHANGE_OTHER_NONCASH = "Change In Other Non-Cash Items"
-    CHANGE_TOTAL_NONCASH = "Change In Total Non-Cash Items"
+    DEFERRED_INCOME_TAXES = "Deferred Income Taxes"
+    CHANGE_OTHER_NONCASH = "Change of Other Non-Cash Items"
+    CHANGE_TOTAL_NONCASH = "Change of Total Non-Cash Items"
     SBC = "Stock-Based Compensation"
-    CHANGE_INVENTORY = "Change In Inventory"
-    CHANGE_ACCOUNTS_RECEIVABLE = "Change In Accounts Receivable"
-    CHANGE_ACCOUNTS_PAYABLE = "Change in Accounts Payable"
-    CHANGE_OTHER_ASSETS_LIABILITIES = "Change In Other Assets and Liabilities"
-    CHANGE_TOTAL_ASSETS_LIABILITIES = "Change In Total Assets and Liabilities"
+    CHANGE_WORKING_CAPITAL = "Change of Working Capital"
+    CHANGE_INVENTORY = "Change of Inventory"
+    CHANGE_ACCOUNTS_RECEIVABLE = "Change of Accounts Receivable"
+    CHANGE_ACCOUNTS_PAYABLE = "Change of Accounts Payable"
+    CHANGE_OTHER_WORKING_CAPITAL = "Change of Other Working Capital"
+    CHANGE_OTHER_ASSETS_LIABILITIES = "Change of Other Assets and Liabilities"
+    CHANGE_TOTAL_ASSETS_LIABILITIES = "Change of Total Assets and Liabilities"
     OPERATING_CASHFLOW = "Operating Cashflow"
     CASHFLOW_PER_SHARE = "Cashflow Per Share"
     CASHFLOW_PER_SHARE_ANALYSTS = "Cashflow Per Share Analyst Count"
 
     CAPEX = "Capital Expenditures"
     CAPEX_ANALYSTS = "Capital Expenditures Analyst Count"
-    CHANGE_ST_SECURITIES = "Change In Short-Term Securities"
-    CHANGE_LT_SECURITIES = "Change In Long-Term Securities"
-    CHANGE_SECURITIES = "Change In Securities"
+    PURCHASE_SECURITIES = "Purchase of Securities"
+    SALE_MATURITY_SECURITIES = "Sales and Maturity of Securities"
+    CHANGE_ST_SECURITIES = "Change of Short-Term Securities"
+    CHANGE_LT_SECURITIES = "Change of Long-Term Securities"
+    CHANGE_SECURITIES = "Change of Securities"
     ACQUISITIONS = "Acquisitions"
-    CHANGE_INTANGIBLE_ASSETS = "Change In Intangible Assets"
+    CHANGE_INTANGIBLE_ASSETS = "Change of Intangible Assets"
     OTHER_INVESTING_ACTIVITIES = "Other Investing Activities"
     INVESTING_CASHFLOW = "Investing Cashflow"
 
@@ -115,6 +142,7 @@ class Conversion(Enum):
     LT_DEBT_ISSUED = "Long-Term Debt Issued"
     ST_DEBT_ISSUED = "Short-Term Debt Issued"
     TOTAL_DEBT_ISSUED = "Total Debt Issued"
+    TOTAL_DEBT_REPAID = "Total Debt Repaid"
     DIVIDENDS_PAID = "Dividends Paid"
     SHARES_REPURCHASED = "Shares Repurchased"
     COMMON_STOCK_ISSUED = "Common Stock Issued"
@@ -125,18 +153,84 @@ class Conversion(Enum):
     FREE_CASHFLOW = "Free Cashflow"
     FREE_CASHFLOW_ANALYSTS = "Free Cashflow Analyst Count"
     CASH_START_PERIOD = "Cash and Cash Equivalents Start of Period"
-    CHANGE_CASH = "Change In Cash and Cash Equivalents"
+    CURRENCY_CASH_IMPACT = "Currency Impact on Cash and Cash Equivalents"
+    CHANGE_CASH = "Change of Cash and Cash Equivalents"
     CASH_END_PRIOD = "Cash and Cash Equivalents End of Period"
 
     # Financial Ratios
+    GROSS_MARGIN = "Gross Margin"
+    EBITDA_MARGIN = "EBITDA Margin"
     OPERATING_MARGIN = "Operating Margin"
     OPERATING_MARGIN_ANALYSTS = "Operating Margin Analyst Count"
+    PRETAX_MARGIN = "Pre-Tax Margin"
     NET_MARGIN = "Net Margin"
     NET_MARGIN_ANALYSTS = "Net Margin Analyst Count"
     FCF_MARGIN = "FCF Margin"
     FCF_MARGIN_ANALYSTS = "FCF Margin Analyst Count"
     FCF_CONVERSION = "FCF Conversion"
     FCF_CONVERSION_ANALYSTS = "FCF Conversion Analyst Count"
+
+    REVENUE_PER_SHARE = "Revenue Per Share"
+    NET_INCOME_PER_SHARE = "Net Income Per Share"
+    OPERATING_CASHFLOW_PER_SHARE = "Operating Cashflow Per Share"
+    FCF_PER_SHARE = "FCF Per Share"
+    CASH_PER_SHARE = "Cash Per Share"
+    TANGIBLE_BOOK_VALUE_PER_SHARE = "Tangible Book Value Per Share"
+    BOOK_VALUE_PER_SHARE = "Book Value Per Share"
+    BOOK_VALUE_PER_SHARE_ANALYSTS = "Book Value Per Share Analyst Count"
+    EQUITY_PER_SHARE = "Equity Per Share",
+    DEBT_PER_SHARE = "Debt Per Share"
+    MARKET_CAP = "Market Capitalization"
+    ENTERPRISE_VALUE = "Enterprise Value"
+
+    PB = "Price/Book Ratio"
+    PS = "Price/Sales Ratio"
+    PE = "Price/Earnings Ratio"
+    PRICE_OPERATING_CASHFLOW = "Price/Operating-Cashflow Ratio"
+    PRICE_FCF = "Price/FCF Ratio"
+    EV_SALES = "Enterprise-Value/Sales Ratio"
+    EV_EBITDA = "Enterprise-Value/EBITDA Ratio"
+    EV_OPERATING_CASHFLOW = "Enterprise-Value/Operating-Cashflow Ratio"
+    EV_FCF = "Enterprise-Value/FCF Ratio"
+    PRICE_TANGIBLE_EQUITY = "Price/Tangible-Equity Ratio"
+    EARNINGS_YIELD = "Earnings Yield"
+    FCF_YIELD = "FCF Yield"
+    DIVIDEND_YIELD = "Dividend Yield"
+
+    DEBT_EQUITY = "Debt/Equity"
+    DEBT_ASSETS = "Debt/Assets"
+    DEBT_EBITDA = "Debt/EBITDA"
+    CURRENT_RATIO = "Current Ratio"
+    INTEREST_COVERAGE = "Interest Coverage"
+    INCOME_QUALITY = "Income Quality"
+    PAYOUT_RATIO = "Payout Ratio"
+    SGA_REVENUE = "SG&A/Revenue"
+    RD_REVENUE = "R&D/Revenue"
+    INTANGIBLES_TOTAL_ASSETS = "Intangibles/Total-Assets"
+    CAPEX_OPERATING_CASHFLOW = "Capex/Operating-Cashflow"
+    CAPEX_REVENUE = "Capex/Revenue"
+    CAPEX_DA = "Capex/Depreciation&Amortization"
+    SBC_REVENUE = "Stock-Based-Compensation/Revenue"
+    GRAHAM_NUMBER = "Graham Number"
+    ROIC = "Return on Invested Capital"
+    RETURN_TANGIBLE_ASSETS = "Return on Tangible Assets"
+    GRAHAM_NET_NET = "Graham Net Net"
+    WORKING_CAPITAL = "Working Capital"
+    TANGIBLE_ASSETS = "Tangible Assets"
+    NET_CURRENT_ASSETS = "Net Current Assets"
+    INVESTED_CAPITAL = "Invested Capital"
+    AVERAGE_RECEIVABLES = "Average Receivables"
+    AVERAGE_PAYABLES = "Average Payables"
+    AVERAGE_INVENTORY = "Average Inventory"
+    DAYS_SALES_OUTSTANDING = "Days of Sales Outstanding"
+    DAYS_PAYABLES_OUTSTANDING = "Days of Payables Outstanding"
+    DAYS_INVENTORY = "Days of Inventory"
+    RECEIVABLE_TURNOVER = "Receivable Turnover"
+    PAYABLES_TURNOVER = "Payables Turnover"
+    INVENTORY_TURNOVER = "Inventory Turnover"
+    ROE = "Return on Equity"
+    CAPEX_PER_SHARE = "Capex Per Share"
+
 
 MACROTRENDS_CONVERSION = {
     # Income Statement
@@ -264,5 +358,175 @@ MARKETSCREENER_CONVERSION = {
         "FCF Margin Analysts": Conversion.FCF_MARGIN_ANALYSTS,
         "FCF Conversion": Conversion.FCF_CONVERSION,
         "FCF Conversion Analysts": Conversion.FCF_CONVERSION_ANALYSTS
+    }
+}
+
+STRATOSPHERE_CONVERSION = {
+    "INCOME_STATEMENT": {
+        "revenue": Conversion.REVENUE,
+        "costOfRevenue": Conversion.COGS,
+        "grossProfit": Conversion.GROSS_PROFIT,
+        "researchAndDevelopmentExpenses": Conversion.RD,
+        "generalAndAdministrativeExpenses": Conversion.GA,
+        "sellingAndMarketingExpenses": Conversion.SM,
+        "sellingGeneralAndAdministrativeExpenses": Conversion.SGA,
+        "otherExpenses": Conversion.OTHER_OPERATING_INCOME,
+        "operatingExpenses": Conversion.TOTAL_OPERATING_EXPENSES,
+        "costAndExpenses": Conversion.TOTAL_EXPENSES,
+        "interestIncome": Conversion.INTEREST_INCOME,
+        "interestExpense": Conversion.INTEREST_EXPENSE,
+        "depreciationAndAmortization": Conversion.DA,
+        "ebitda": Conversion.EBITDA,
+        "operatingIncome": Conversion.OPERATING_INCOME,
+        "totalOtherIncomeExpensesNet": Conversion.OTHER_INCOME,
+        "incomeBeforeTax": Conversion.PRETAX_INCOME,
+        "incomeTaxExpense": Conversion.INCOME_TAXES,
+        "netIncome": Conversion.NET_INCOME,
+        "eps": Conversion.EPS_BASIC,
+        "epsdiluted": Conversion.EPS_DILUTED,
+        "weightedAverageShsOut": Conversion.SHARES_BASIC,
+        "weightedAverageShsOutDil": Conversion.SHARES_DILUTED
+    },
+    "BALANCE_SHEET": {
+        "cashAndCashEquivalents": Conversion.CASH,
+        "shortTermInvestments": Conversion.ST_SECURITIES,
+        "cashAndShortTermInvestments": Conversion.CASH_ST_SECURITIES,
+        "netReceivables": Conversion.ACCOUNTS_RECEIVABLE,
+        "inventory": Conversion.INVENTORY,
+        "otherCurrentAssets": Conversion.OTHER_CURRENT_ASSETS,
+        "totalCurrentAssets": Conversion.TOTAL_CURRENT_ASSETS,
+        "propertyPlantEquipmentNet": Conversion.PROPERTY_PLANT_EQUIPMENT,
+        "goodwill": Conversion.GOODWILL,
+        "intangibleAssets": Conversion.INTANGIBLE_ASSETS,
+        "goodwillAndIntangibleAssets": Conversion.GOODWILL_INTANGIBLES,
+        "longTermInvestments": Conversion.LT_SECURITIES,
+        "taxAssets": Conversion.TAX_ASSETS,
+        "otherNonCurrentAssets": Conversion.OTHER_NONCURRENT_ASSETS,
+        "totalNonCurrentAssets": Conversion.TOTAL_NONCURRENT_ASSETS,
+        "otherAssets": Conversion.OTHER_ASSETS,
+        "totalAssets": Conversion.TOTAL_ASSETS,
+        "accountPayables": Conversion.ACCOUNTS_PAYABLE,
+        "shortTermDebt": Conversion.ST_DEBT,
+        "taxPayables": Conversion.TAX_LIABILITIES,
+        "deferredRevenue": Conversion.ST_DEFERRED_REVENUE,
+        "otherCurrentLiabilities": Conversion.OTHER_CURRENT_LIABILITIES,
+        "totalCurrentLiabilities": Conversion.TOTAL_CURRENT_LIABILITIES,
+        "longTermDebt": Conversion.LT_DEBT,
+        "deferredRevenueNonCurrent": Conversion.LT_DEFERRED_REVENUE,
+        "deferredTaxLiabilitiesNonCurrent": Conversion.LT_DEFERRED_TAX_LIABILITIES,
+        "otherNonCurrentLiabilities": Conversion.OTHER_NONCURRENT_LIABILITIES,
+        "totalNonCurrentLiabilities": Conversion.TOTAL_NONCURRENT_LIABILITIES,
+        "otherLiabilities": Conversion.OTHER_LIABILITIES,
+        "capitalLeaseObligations": Conversion.CAPITAL_LEASE_OBLIGATIONS,
+        "totalLiabilities": Conversion.TOTAL_LIABILITIES,
+        "preferredStock": Conversion.PREFERRED_STOCK,
+        "commonStock": Conversion.COMMON_STOCK,
+        "retainedEarnings": Conversion.RETAINED_EARNINGS,
+        "accumulatedOtherComprehensiveIncomeLoss": Conversion.COMPREHENSIVE_INCOME,
+        "othertotalStockholdersEquity": Conversion.OTHER_SHAREHOLDERS_EQUITY,
+        "totalStockholdersEquity": Conversion.TOTAL_SHAREHOLDERS_EQUITY,
+        "totalEquity": Conversion.TOTAL_EQUITY,
+        "totalLiabilitiesAndStockholdersEquity": Conversion.TOTAL_LIABILITIES_AND_SHAREHOLDERS_EQUITY,
+        "minorityInterest": Conversion.MINORITY_INTEREST,
+        "totalLiabilitiesAndTotalEquity": Conversion.TOTAL_LIABILITIES_AND_EQUITY,
+        "totalInvestments": Conversion.TOTAL_INVESTMENTS,
+        "totalDebt": Conversion.TOTAL_DEBT,
+        "netDebt": Conversion.NET_DEBT
+    },
+    "CASHFLOW_STATEMENT": {
+        "netIncome": Conversion.NET_INCOME_CF,
+        "depreciationAndAmortization": Conversion.DA,
+        "deferredIncomeTax": Conversion.DEFERRED_INCOME_TAXES,
+        "stockBasedCompensation": Conversion.SBC,
+        "changeInWorkingCapital": Conversion.CHANGE_WORKING_CAPITAL,
+        "accountsReceivables": Conversion.CHANGE_ACCOUNTS_RECEIVABLE,
+        "inventory": Conversion.CHANGE_INVENTORY,
+        "accountsPayables": Conversion.CHANGE_ACCOUNTS_PAYABLE,
+        "otherWorkingCapital": Conversion.CHANGE_OTHER_WORKING_CAPITAL,
+        "otherNonCashItems": Conversion.CHANGE_OTHER_NONCASH,
+        "netCashProvidedByOperatingActivities": Conversion.OPERATING_CASHFLOW,
+        "investmentsInPropertyPlantAndEquipment": Conversion.CAPEX,
+        "acquisitionsNet": Conversion.ACQUISITIONS,
+        "purchasesOfInvestments": Conversion.PURCHASE_SECURITIES,
+        "salesMaturitiesOfInvestments": Conversion.SALE_MATURITY_SECURITIES,
+        "otherInvestingActivites": Conversion.OTHER_INVESTING_ACTIVITIES,
+        "netCashUsedForInvestingActivites": Conversion.INVESTING_CASHFLOW,
+        "debtRepayment": Conversion.TOTAL_DEBT_REPAID,
+        "commonStockIssued": Conversion.TOTAL_STOCK_ISSUED,
+        "commonStockRepurchased": Conversion.SHARES_REPURCHASED,
+        "dividendsPaid": Conversion.DIVIDENDS_PAID,
+        "otherFinancingActivites": Conversion.OTHER_FINANCING_ACTIVITIES,
+        "netCashUsedProvidedByFinancingActivities": Conversion.FINANCING_CASHFLOW,
+        "effectOfForexChangesOnCash": Conversion.CURRENCY_CASH_IMPACT,
+        "netChangeInCash": Conversion.CHANGE_CASH,
+        "cashAtEndOfPeriod": Conversion.CASH_END_PRIOD,
+        "cashAtBeginningOfPeriod": Conversion.CASH_START_PERIOD,
+        "operatingCashFlow": Conversion.OPERATING_CASHFLOW,
+        "capitalExpenditure": Conversion.CAPEX,
+        "freeCashFlow": Conversion.FREE_CASHFLOW
+    },
+    "FINANCIAL_RATIOS": {
+        "grossProfitRatio": Conversion.GROSS_MARGIN,
+        "ebitdaRatio": Conversion.EBITDA_MARGIN,
+        "operatingIncomeRatio": Conversion.OPERATING_MARGIN,
+        "incomeBeforeTaxRatio": Conversion.PRETAX_MARGIN,
+        "netIncomeRatio": Conversion.NET_MARGIN,
+        "revenuePerShare": Conversion.REVENUE_PER_SHARE,
+        "netIncomePerShare": Conversion.NET_INCOME_PER_SHARE,
+        "operatingCashFlowPerShare": Conversion.OPERATING_CASHFLOW_PER_SHARE,
+        "freeCashFlowPerShare": Conversion.FCF_PER_SHARE,
+        "cashPerShare": Conversion.CASH_PER_SHARE,
+        "bookValuePerShare": Conversion.BOOK_VALUE_PER_SHARE,
+        "tangibleBookValuePerShare": Conversion.TANGIBLE_BOOK_VALUE_PER_SHARE,
+        "shareholdersEquityPerShare": Conversion.EQUITY_PER_SHARE,
+        "interestDebtPerShare": Conversion.DEBT_PER_SHARE,
+        "marketCap": Conversion.MARKET_CAP,
+        "enterpriseValue": Conversion.ENTERPRISE_VALUE,
+        "peRatio": Conversion.PE,
+        "priceToSalesRatio": Conversion.PS,
+        "pocfratio": Conversion.PRICE_OPERATING_CASHFLOW,
+        "pfcfRatio": Conversion.PRICE_FCF,
+        "pbRatio": Conversion.PB,
+        "ptbRatio": Conversion.PRICE_TANGIBLE_EQUITY,
+        "evToSales": Conversion.EV_SALES,
+        "enterpriseValueOverEBITDA": Conversion.EV_EBITDA,
+        "evToOperatingCashFlow": Conversion.EV_OPERATING_CASHFLOW,
+        "evToFreeCashFlow": Conversion.EV_FCF,
+        "earningsYield": Conversion.EARNINGS_YIELD,
+        "freeCashFlowYield": Conversion.FCF_YIELD,
+        "debtToEquity": Conversion.DEBT_EQUITY,
+        "debtToAssets": Conversion.DEBT_ASSETS,
+        "netDebtToEBITDA": Conversion.DEBT_EBITDA,
+        "currentRatio": Conversion.CURRENT_RATIO,
+        "interestCoverage": Conversion.INTEREST_COVERAGE,
+        "incomeQuality": Conversion.INCOME_QUALITY,
+        "dividendYield": Conversion.DIVIDEND_YIELD,
+        "payoutRatio": Conversion.PAYOUT_RATIO,
+        "salesGeneralAndAdministrativeToRevenue": Conversion.SGA_REVENUE,
+        "researchAndDdevelopementToRevenue": Conversion.RD_REVENUE,
+        "intangiblesToTotalAssets": Conversion.INTANGIBLES_TOTAL_ASSETS,
+        "capexToOperatingCashFlow": Conversion.CAPEX_OPERATING_CASHFLOW,
+        "capexToRevenue": Conversion.CAPEX_REVENUE,
+        "capexToDepreciation": Conversion.CAPEX_DA,
+        "stockBasedCompensationToRevenue": Conversion.SBC_REVENUE,
+        "grahamNumber": Conversion.GRAHAM_NUMBER,
+        "roic": Conversion.ROIC,
+        "returnOnTangibleAssets": Conversion.RETURN_TANGIBLE_ASSETS,
+        "grahamNetNet": Conversion.GRAHAM_NET_NET,
+        "workingCapital": Conversion.WORKING_CAPITAL,
+        "tangibleAssetValue": Conversion.TANGIBLE_ASSETS,
+        "netCurrentAssetValue": Conversion.NET_CURRENT_ASSETS,
+        "investedCapital": Conversion.INVESTED_CAPITAL,
+        "averageReceivables": Conversion.AVERAGE_RECEIVABLES,
+        "averagePayables": Conversion.AVERAGE_PAYABLES,
+        "averageInventory": Conversion.AVERAGE_INVENTORY,
+        "daysSalesOutstanding": Conversion.DAYS_SALES_OUTSTANDING,
+        "daysPayablesOutstanding": Conversion.DAYS_PAYABLES_OUTSTANDING,
+        "daysOfInventoryOnHand": Conversion.DAYS_INVENTORY,
+        "receivablesTurnover": Conversion.RECEIVABLE_TURNOVER,
+        "payablesTurnover": Conversion.PAYABLES_TURNOVER,
+        "inventoryTurnover": Conversion.INVENTORY_TURNOVER,
+        "roe": Conversion.ROE,
+        "capexPerShare": Conversion.CAPEX_PER_SHARE
     }
 }
