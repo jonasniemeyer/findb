a6d34c2a0fe01bca821bc03c85937f5a5b148f3a
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-03-03 09:37:51 +0100

    add filing_url and list_id insert

--- daily_update/update_sec_filings.py
+++ daily_update/update_sec_filings.py
@@ -17,7 +17,7 @@ def get_sec_filing_lists(start_year=1900, start_quarter=0):
             for quarter in quarters["directory"]["item"]:
                 if int(quarter["name"][-1]) < start_quarter and int(year["name"]) == start_year:
                     continue
-                print(f"  {quarter['name']}")
+                print(f"{quarter['name']}")
 
                 quarter_url = f"{SEC_BASE_URL}/edgar/daily-index/{year['name']}/{quarter['name']}/index.json"
                 days = requests.get(url=quarter_url, headers=HEADERS).json()
@@ -40,33 +40,35 @@ def get_sec_filing_lists(start_year=1900, start_quarter=0):
                 con.commit()
 
 def scrape_sec_filing_lists():
-    filing_lists = cur.execute("SELECT url FROM sec_daily_list WHERE parsed = ?", (False,)).fetchall()
-    filing_lists = [item[0] for item in filing_lists]
+    filing_lists = cur.execute("SELECT list_id, url FROM sec_daily_list WHERE parsed = ?", (False,)).fetchall()
     total_filings = len(filing_lists)
+    trail = len(str(total_filings))
 
-    for index, url in enumerate(filing_lists):
-        print(f"    {index+1} of {total_filings}: {url}")
+    for index, (list_id, url) in enumerate(filing_lists):
+        print(f"{index+1:>{trail}} of {total_filings}: {url}")
         file = requests.get(url=url, headers=HEADERS).text.split("\n")
         for row in file:
             row = row.split("|")
             if len(row) != 5 or any([item == "" for item in row]):
                 continue
             cik, _, form, date, href = row
-            if cik == "CIK":
+            if cik.strip() == "CIK":
                 continue
             cik = int(cik)
             date = f"{date[:4]}-{date[4:6]}-{date[6:]}"
             ts_filed = int(pd.to_datetime(date).timestamp())
             href_split = href.strip().split("/")
-            href = f"{SEC_BASE_URL}/edgar/data/{href_split[-2]}/{href_split[-1]}"
+            document_url = f"{SEC_BASE_URL}/edgar/data/{href_split[-2]}/{href_split[-1]}"
+            accession = href_split[-1].split(".")[0].replace("-", "")
+            filing_url = f"{SEC_BASE_URL}/edgar/data/{href_split[-2]}/{accession}/{href_split[-1].replace('.txt', '-index.html')}"
             cur.execute("INSERT OR IGNORE INTO sec_form_type (name) VALUES (?)", (form,))
             form_id = cur.execute("SELECT type_id FROM sec_form_type WHERE name = ?", (form,)).fetchone()[0]
             cur.execute(
                 """
-                INSERT INTO sec_filing (cik, form_type_id, ts_filed, url, parsed)
-                VALUES (?, ?, ?, ?, ?)
+                INSERT OR IGNORE INTO sec_filing (cik, type_id, ts_filed, filing_url, document_url, parsed, list_id)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
                 """,
-                (cik, form_id, ts_filed, href, False)
+                (cik, form_id, ts_filed, filing_url, document_url, False, list_id)
             )
         cur.execute("UPDATE sec_daily_list SET parsed = ? WHERE url = ?", (True, url))
         con.commit()
@@ -79,6 +81,7 @@ if __name__ == "__main__":
     filing_lists = cur.execute("SELECT ts, url FROM sec_daily_list ORDER BY ts DESC LIMIT 1").fetchall()
 
     # if there are no filing lists in the database, scrape all lists. Else, scrape only those that are dated later
+    print("Scraping SEC Daily Filing Lists")
     if len(filing_lists) == 0:
         get_sec_filing_lists()
     else:
@@ -87,5 +90,6 @@ if __name__ == "__main__":
         quarter = (last_date_parsed.month - 1) // 3 + 1
         get_sec_filing_lists(start_year=year, start_quarter=quarter)
     
+    print("Scraping SEC Filings")
     scrape_sec_filing_lists()
     con.close()
