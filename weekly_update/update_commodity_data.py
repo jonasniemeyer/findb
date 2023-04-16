7986caad41fa844ab2b302c430ba323cc46d37c7
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-04-16 14:26:01 +0200

    rm register_adapter, use Database context manager

--- weekly_update/update_commodity_data.py
+++ weekly_update/update_commodity_data.py
@@ -1,38 +1,27 @@
 from finance_database import Database
 from finance_data import CMEReader
 import pandas as pd
-import numpy as np
-from sqlite3 import register_adapter
 import datetime as dt
 
 if dt.date.today().weekday() == 6:
+    with Database() as db:
+        ts_today = int(pd.to_datetime(pd.to_datetime("today").date()).timestamp())
 
-    register_adapter(np.int64, lambda val: int(val))
-    register_adapter(np.float64, lambda val: float(val))
+        data = {}
+        length = len(CMEReader.commodities)
+        trail = len(str(length))
 
-    ts_today = int(pd.to_datetime(pd.to_datetime("today").date()).timestamp())
+        for index, commodity in enumerate(CMEReader.commodities.keys()):
+            print(f"{index+1:{trail}} of {length}: {commodity}")
+            data[commodity] = CMEReader(commodity, timestamps=True).read()
 
-    db = Database()
-    con = db.connection
-    cur = db.cursor
-
-    data = {}
-    length = len(CMEReader.commodities)
-    for index, commodity in enumerate(CMEReader.commodities.keys()):
-        print(f"{index+1} of {length}: {commodity}")
-        data[commodity] = CMEReader(commodity, timestamps=True).read()
-
-    for commodity in data.keys():
-        commodity_id = cur.execute("SELECT commodity_id FROM cme_commodity WHERE name = ?", (commodity,)).fetchone()[0]
-        for ts in data[commodity].keys():
-            df = data[commodity][ts]
-            df["commodity_id"] = commodity_id
-            df["maturity_ts"] = df.index
-            df["ts"] = ts
-            df = df[["commodity_id", "maturity_ts", "ts", "Settle", "Volume", "Open Interest"]].to_numpy()
-            cur.executemany("REPLACE INTO cme_commodity_data VALUES (?, ?, ?, ?, ?, ?)", df)
-        
-        cur.execute("UPDATE cme_commodity SET prices_updated = ? WHERE name = ?", (ts_today, commodity))
-
-    con.commit()
-    con.close()
+        for commodity in data.keys():
+            commodity_id = db.cur.execute("SELECT commodity_id FROM cme_commodity WHERE name = ?", (commodity,)).fetchone()[0]
+            for ts in data[commodity].keys():
+                df = data[commodity][ts]
+                df["commodity_id"] = commodity_id
+                df["maturity_ts"] = df.index
+                df["ts"] = ts
+                df = df[["commodity_id", "maturity_ts", "ts", "Settle", "Volume", "Open Interest"]].to_numpy()
+                db.cur.executemany("REPLACE INTO cme_commodity_data VALUES (?, ?, ?, ?, ?, ?)", df)
+            db.cur.execute("UPDATE cme_commodity SET prices_updated = ? WHERE name = ?", (ts_today, commodity))
